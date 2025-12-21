3352e24bc6b50b870b09867f76700205d8986e2c
BatogovRoman <romanbatogov@gmail.com>
2025-12-21 14:45:04 +0500

    Task0

--- Tyuiu.BatogovRK.Sprint6.Task0.V13.Lib/DataService.cs
+++ Tyuiu.BatogovRK.Sprint6.Task0.V13.Lib/DataService.cs
@@ -6,7 +6,6 @@ namespace Tyuiu.BatogovRK.Sprint6.Task0.V13.Lib
         public double Calculate(int x)
         {
             double res = Math.Pow(x, 3) / (Math.Pow(x, 2) - 1);
-
             return Math.Round(res, 3);
         }
     }
