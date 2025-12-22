e7dda65ad6531b20b3054e367ae83d31a12feb47
BatogovRoman <romanbatogov@gmail.com>
2025-12-22 17:10:30 +0500

    Task 7

---
+++ Tyuiu.BatogovRK.Sprint6.Task7.V12/FormMain.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Tyuiu.BatogovRK.Sprint6.Task7.V12
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewIn = new DataGridView();
+            dataGridViewOut = new DataGridView();
+            openFileDialog = new OpenFileDialog();
+            textBoxInPut = new TextBox();
+            textBoxOutPut = new TextBox();
+            buttonClick = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewIn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOut).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewIn
+            // 
+            dataGridViewIn.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewIn.Location = new Point(17, 43);
+            dataGridViewIn.Margin = new Padding(4, 5, 4, 5);
+            dataGridViewIn.Name = "dataGridViewIn";
+            dataGridViewIn.RowHeadersWidth = 62;
+            dataGridViewIn.Size = new Size(311, 662);
+            dataGridViewIn.TabIndex = 0;
+            // 
+            // dataGridViewOut
+            // 
+            dataGridViewOut.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewOut.Location = new Point(353, 43);
+            dataGridViewOut.Margin = new Padding(4, 5, 4, 5);
+            dataGridViewOut.Name = "dataGridViewOut";
+            dataGridViewOut.RowHeadersWidth = 62;
+            dataGridViewOut.Size = new Size(301, 662);
+            dataGridViewOut.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = "openFileDialog1";
+            openFileDialog.FileOk += openFileDialog1_FileOk;
+            // 
+            // textBoxInPut
+            // 
+            textBoxInPut.Location = new Point(749, 102);
+            textBoxInPut.Margin = new Padding(4, 5, 4, 5);
+            textBoxInPut.Name = "textBoxInPut";
+            textBoxInPut.Size = new Size(288, 31);
+            textBoxInPut.TabIndex = 2;
+            // 
+            // textBoxOutPut
+            // 
+            textBoxOutPut.Location = new Point(749, 185);
+            textBoxOutPut.Margin = new Padding(4, 5, 4, 5);
+            textBoxOutPut.Name = "textBoxOutPut";
+            textBoxOutPut.Size = new Size(288, 31);
+            textBoxOutPut.TabIndex = 3;
+            // 
+            // buttonClick
+            // 
+            buttonClick.Location = new Point(749, 385);
+            buttonClick.Margin = new Padding(4, 5, 4, 5);
+            buttonClick.Name = "buttonClick";
+            buttonClick.Size = new Size(290, 77);
+            buttonClick.TabIndex = 4;
+            buttonClick.Text = "Открыть файл";
+            buttonClick.UseVisualStyleBackColor = true;
+            buttonClick.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(749, 523);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(290, 77);
+            button1.TabIndex = 5;
+            button1.Text = "Обработать файл";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 750);
+            Controls.Add(button1);
+            Controls.Add(buttonClick);
+            Controls.Add(textBoxOutPut);
+            Controls.Add(textBoxInPut);
+            Controls.Add(dataGridViewOut);
+            Controls.Add(dataGridViewIn);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "FormMain";
+            Text = "Спринт 6 | Таск 7 | Вариант 12 | Батогов Р.К.";
+            Load += FormMain_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewIn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOut).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewIn;
+        private DataGridView dataGridViewOut;
+        private OpenFileDialog openFileDialog;
+        private TextBox textBoxInPut;
+        private TextBox textBoxOutPut;
+        private Button buttonClick;
+        private Button button1;
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint6.Task7.V12/FormMain.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows.Forms;
+using Tyuiu.BatogovRK.Sprint6.Task7.V12.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint6.Task7.V12
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog.Title = "Выберите файл";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(path);
+                if (lines.Length == 0)
+                {
+                    MessageBox.Show("Ошибка");
+                    return;
+                }
+                dataGridViewIn.Rows.Clear();
+                dataGridViewIn.Columns.Clear();
+                string[] firstrow = lines[0].Split(';');
+                for (int i = 0; i < firstrow.Length; i++)
+                {
+                    dataGridViewIn.Columns.Add($"Column{i + 1}", $"Column{i + 1}");
+                }
+                foreach (string line in lines)
+                {
+                    string[] row = line.Split(';');
+                    dataGridViewIn.Rows.Add(row);
+                }
+            }
+        }
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string path = openFileDialog.FileName;
+            DataService ds = new DataService();
+            int[,] matrix = ds.GetMatrix(path);
+            int rowcount = 10;
+            int columncount = 10;
+            for (int i = 0; i < columncount; i++)
+            {
+                dataGridViewOut.Columns.Add($"Column{i + 1}", $"Column{i + 1}");
+            }
+            for (int i = 0; i < rowcount; i++)
+            {
+                object[] newmatrix = new object[columncount];
+                for (int j = 0; j < columncount; j++)
+                {
+                    newmatrix[j] = matrix[i, j];
+                }
+                dataGridViewOut.Rows.Add(newmatrix);
+            }
+        }
+    }
+}
